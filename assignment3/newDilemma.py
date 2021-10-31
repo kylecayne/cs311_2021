8f1fe12807261a948741cfc1035d4a6dbabc3863
kylecayne <35715660+kylecayne@users.noreply.github.com>
2021-10-31 16:59:49 -0700

    Update newDilemma.py

--- assignment3/newDilemma.py
+++ assignment3/newDilemma.py
@@ -25,7 +25,7 @@ parser.add_argument('--iterations', help='number of iterations in game')
 parser.add_argument('--last_opponent_move', help='last opponent move')
 args = parser.parse_args()
 
-myfile = open("info.json")
+myfile = open("info.json", "a+")
 
 data = json.load(myfile)
 
